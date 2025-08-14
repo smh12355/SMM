7d95b212885fb1cc08f82262f7e00cc9ba920ad0
BogdanS. <krik6162@gmail.com>
2025-08-14 17:47:01 +0500

    Enhance logging and error handling in WebApi and Persistence
    Added detailed logging configuration for EF Core and ASP.NET Core in Program.cs, including console and debug providers, log filters, and HTTP logging. Introduced a global error handler with logging. Updated AddPersistence to enable EF Core sensitive data logging, detailed errors, and logger factory usage.

--- src/SMM.Persistence/DependencyInjection.cs
+++ src/SMM.Persistence/DependencyInjection.cs
@@ -1,25 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 using Smm.Application.Common.Interfaces;
-using System;
 
 namespace SMM.Persistence;
 
 public static class DependencyInjection
 {
-    /// <summary>
-    /// Extension method for persistence method
-    /// </summary>
-    /// <param name="services">Services</param>
-    /// <param name="configuration">Configuration</param>
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<SmmDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("Postgres")));
+        services.AddDbContext<SmmDbContext>((serviceProvider, options) =>
+        {
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            options
+                .UseNpgsql(configuration.GetConnectionString("Postgres"))
+                .UseLoggerFactory(loggerFactory)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        });
 
         services.AddScoped<ISmmDbContext>(provider => provider.GetRequiredService<SmmDbContext>());
-
+         
         return services;
     }
 }
--- src/SMM.WebApi/Program.cs
+++ src/SMM.WebApi/Program.cs
@@ -1,18 +1,57 @@
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+
 using SMM.CompositionRoot;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSmmServices(builder.Configuration);
+// Чистим провайдеры и добавляем консоль
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
+// Фильтры для подробных логов EF и ASP.NET Core
+builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Information);
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Debug);
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Connection", LogLevel.Debug);
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Query", LogLevel.Debug);
+
+// Добавляем HTTP логирование
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+});
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestPath
+                            | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponseStatusCode;
+});
+// Add services to the container.
+builder.Services.AddSmmServices(builder.Configuration); 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Глобальный обработчик ошибок с логированием
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        var exception = context.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerFeature>()?.Error;
+
+        logger.LogError(exception, "Произошла необработанная ошибка");
+
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("Произошла ошибка");
+    });
+});
+
+// Configure the HTTP request pipeline. 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,9 +59,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
